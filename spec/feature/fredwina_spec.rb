f9637f0099a068ca48cf00b15bc886456dfd1b47
Adrien Bourgeois <adrienfrancis.bourgeois@gmail.com>
2015-01-16 13:32:50 +1100

    define feature specs

--- spec/feature/fredwina_spec.rb
+++ spec/feature/fredwina_spec.rb
@@ -1,5 +1,19 @@
 RSpec.describe 'fredwina' do
-  describe 'dummy test' do
-    it { expect(1 == 1).to eq(true) }
+  context "dogs stay inside the paddock and don't bump into each other" do
+    it "returns the final positions of the dogs" do
+      expect(Fredwina.move("../fixtures/input_example_success.text")).to eq("1 3 N\n5 1 E")
+    end
+  end
+
+  context "at least one dog tries to go outside the paddock" do
+    it "returns an appropriate error message" do
+      expect(Fredwina.move("../fixtures/input_example_error1.text")).to eq("The dogs have to stay inside of the paddock")
+    end
+  end
+
+  context "at least one dog bump into another dog" do
+    it "returns an appropriate error message" do
+      expect(Fredwina.move("../fixtures/input_example_error2.text")).to eq("The dogs cannot bump into each other")
+    end
   end
 end
