f00874d81007cfd6fb068d36252b73d8ec560898
Adrien Bourgeois <adrienfrancis.bourgeois@gmail.com>
2015-01-16 14:52:04 +1100

    Dog specs

---
+++ spec/model/dog_spec.rb
@@ -0,0 +1,38 @@
+require_relative '../../config'
+
+RSpec.describe 'dog' do
+
+  describe 'move' do
+    subject { Dog.new([1,2,'N']) }
+
+    it { expect(subject).to respond_to(:move) }
+
+    it 'moves the dog to the right coord' do
+      subject.move
+      expect(subject.x).to eq(2)
+      expect(subject.y).to eq(2)
+      expect(subject.orientation).to eq('N')
+    end
+  end
+
+  describe 'turn' do
+    subject { Dog.new([1,2,'N']) }
+    it { expect(subject).to respond_to(:turn) }
+
+    context 'turn to the left' do
+      subject { Dog.new([1,2,'N']) }
+      it "changes the dog's orientation" do
+        subject.turn('L')
+        expect(subject.orientation).to eq('W')
+      end
+    end
+
+    context 'turn to the left' do
+      subject { Dog.new([1,2,'N']) }
+      it "changes the dog's orientation" do
+        subject.turn('R')
+        expect(subject.orientation).to eq('E')
+      end
+    end
+  end
+end
